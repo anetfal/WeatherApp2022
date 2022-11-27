83b7d6e68ec872a3f31489fd35432a4d2b3dbc5b
anetfal <114697336+anetfal@users.noreply.github.com>
2022-11-27 18:39:39 +0000

    Update index.js
    Temp conversion

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+let showTempInC = true;
+let tempInC = 19;
 let apiKey = "96ad27349a64ea1dcdfbe6f4d458c085";
 let dayNames = [
    'Sunday',
@@ -17,7 +19,12 @@ function setDateTime() {
 
 function setWeatherFromApiResponse(response) {
    let h2 = document.querySelector('h2');
-   h2.innerHTML = `${Math.round(response.data.main.temp)}°C`;
+   tempInC = response.data.main.temp;
+   if (showTempInC) {
+      h2.innerHTML = `${Math.round(tempInC)}°C`;
+   } else {
+      h2.innerHTML = `${Math.round(cToF(tempInC))}°F`;
+   }
 }
 
 function onFormSubmit(event) {
@@ -47,9 +54,43 @@ function currentLocationOnClick() {
    navigator.geolocation.getCurrentPosition(setWeatherFromPosition);
 }
 
+function fToC(tempInF) {
+   return (tempInF - 32) / (9 / 5);
+}
+
+function cToF(tempInC) {
+   return tempInC * 9/5 + 32;
+}
+
 setDateTime();
 let form = document.querySelector('form');
 form.addEventListener('submit', onFormSubmit);
 
 let currentLocationButton = document.querySelector('#current-location');
 currentLocationButton.addEventListener('click', currentLocationOnClick);
+
+function setTempToC() {
+   if (showTempInC === true) {
+      return;
+   }
+
+   showTempInC = true;
+   let h2 = document.querySelector('h2');
+   h2.innerHTML = `${Math.round(tempInC)}°C`
+}
+
+let cLink = document.getElementById('set-to-c');
+cLink.addEventListener('click', setTempToC);
+
+function setTempToF() {
+   if (showTempInC === false) {
+      return;
+   }
+
+   showTempInC = false;
+   let h2 = document.querySelector('h2');
+   h2.innerHTML = `${Math.round(cToF(tempInC))}°F`
+}
+
+let fLink = document.getElementById('set-to-f');
+fLink.addEventListener('click', setTempToF);
